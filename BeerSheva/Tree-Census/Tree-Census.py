d3187799061adda366149113da839bfa06a1de19
yamtimor <yamtimor@gmail.com>
2023-02-13 17:44:21 +0200

    decide to get data from the api without the package

--- BeerSheva/Tree-Census/Tree-Census.py
+++ BeerSheva/Tree-Census/Tree-Census.py
@@ -1,8 +1,4 @@
 import pandas as pd
 import streamlit
 import requests
-# import Ckan-API-Python-Wrapper
 import os
-
-# res = requests.get("https://data.gov.il/api/3/action/datastore_search?resource_id=b7bfe395-a977-452a-8429-e258b71c5b10")
-# print(res.status_code)
