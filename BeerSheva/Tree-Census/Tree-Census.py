4882d8c95589341670b05f4cd98f43ed1a747f5b
yamtimor <yamtimor@gmail.com>
2023-07-14 20:27:23 +0300

    transform to df

--- BeerSheva/Tree-Census/Tree-Census.py
+++ BeerSheva/Tree-Census/Tree-Census.py
@@ -17,8 +17,13 @@ def get_data(url):
 def extract_records(data):
     return data['result']['records']
 
+def record_to_df(records):
+    df = pd.DataFrame(records)
+    return df
+
 if __name__ == "__main__":
     url = "https://data.gov.il/api/3/action/datastore_search?resource_id=b7bfe395-a977-452a-8429-e258b71c5b10&limit=5"
     data = get_data(url)
     records = extract_records(data)
-    pprint(records)
+    df = record_to_df(records)
+    print(df)
