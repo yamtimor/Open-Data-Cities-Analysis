6dbf9f824d813b6964cc01e4ba650092f0662acc
yamtimor <yamtimor@gmail.com>
2023-07-14 20:24:48 +0300

    Extraction Succeed

--- BeerSheva/Tree-Census/Tree-Census.py
+++ BeerSheva/Tree-Census/Tree-Census.py
@@ -2,14 +2,19 @@ import pandas as pd
 import streamlit
 import requests
 import os
+from pprint import pprint
 
 
-# +
 def get_data(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
+    try:
+        response = requests.get(url)
+        data = response.json()
+        return data
+    except Exception as e:
+        print(f"Error occurred: {e}")
         return None
 
-get_data()
+if __name__ == "__main__":
+    url = "https://data.gov.il/api/3/action/datastore_search?resource_id=b7bfe395-a977-452a-8429-e258b71c5b10&limit=5"
+    data = get_data(url)
+    pprint(data['result']['records'])
