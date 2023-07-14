1933ce0664db2bd1c24eea57a37ead77353f8b56
yamtimor <yamtimor@gmail.com>
2023-07-14 20:26:07 +0300

    extract records

--- BeerSheva/Tree-Census/Tree-Census.py
+++ BeerSheva/Tree-Census/Tree-Census.py
@@ -14,7 +14,11 @@ def get_data(url):
         print(f"Error occurred: {e}")
         return None
 
+def extract_records(data):
+    return data['result']['records']
+
 if __name__ == "__main__":
     url = "https://data.gov.il/api/3/action/datastore_search?resource_id=b7bfe395-a977-452a-8429-e258b71c5b10&limit=5"
     data = get_data(url)
-    pprint(data['result']['records'])
+    records = extract_records(data)
+    pprint(records)
