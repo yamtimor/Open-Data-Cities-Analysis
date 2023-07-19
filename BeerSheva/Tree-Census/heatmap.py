21e6f3e2a10d1a765c3c252676fa7bc53edad4a2
yamtimor <yamtimor@gmail.com>
2023-07-19 20:21:38 +0300

    add documentation

--- BeerSheva/Tree-Census/heatmap.py
+++ BeerSheva/Tree-Census/heatmap.py
@@ -2,6 +2,12 @@ import folium
 from folium.plugins import HeatMap
 
 def create_heatmap(data):
+    """
+    Create a heatmap from a dataframe
+    :param data: zipped lat and long series from the dataframe
+    :return: heatmap
+    """
+
     # create a heat map
     heatmap = folium.Map(location=[31.2517, 34.7912], zoom_start=13)
 
